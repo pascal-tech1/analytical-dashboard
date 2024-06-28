aaba6d3594b8141f00ac5cd303875eb295ff6c05
Azubike Pascal <pascalazubike004@gmail.com>
2024-06-28 11:30:20 -0700

    fixing fetching error

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "https://analytical-dashboard-coral.vercel.app",
+  baseURL: "https://analytical-dashboard-git-main-pascals-projects-4016dc21.vercel.app",
 });
 
 export default customFetch;
