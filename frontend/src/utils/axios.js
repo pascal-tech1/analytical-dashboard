309754810f29834d3a566fa83b0fd7d257625a67
Azubike Pascal <pascalazubike004@gmail.com>
2024-06-28 14:56:30 -0700

    update axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const customFetch = axios.create({
   baseURL:
-    "https://analytical-dashboard-git-main-pascals-projects-4016dc21.vercel.app",
+    "https://analytical-dashboard-o245tgj3z-pascals-projects-4016dc21.vercel.app",
 });
 
 export default customFetch;
