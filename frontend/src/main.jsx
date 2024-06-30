f98f2b085e765b521df8329db442c59e3b99060c
AZUBIKE PASCAL ADOH <123998334+pascal-tech1@users.noreply.github.com>
2024-06-30 09:20:44 -0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { store } from "./redux/Store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
     <App />
   </Provider>
 );
